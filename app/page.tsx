b3edcc7cbca7a1f08e8e91f1bf7c529462e208b9
Michael Frieze <michaelfrieze@icloud.com>
2024-01-29 21:42:55 -0500

    build: install and setup shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">Visual Workspace Home Page</h1>
+      <Button size={"lg"}>Click Me</Button>
     </div>
   );
 }
