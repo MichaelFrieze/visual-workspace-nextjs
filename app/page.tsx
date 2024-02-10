8e5d9c113d1c15474a78b210f75c008b715f89be
Michael Frieze <michaelfrieze@icloud.com>
2024-02-10 17:12:01 -0500

    feat: add Convex and Clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <Button size={"lg"}>Click Me</Button>
+    <div className="flex flex-col gap-y-4">
+      <h1>This is a screen for authenticated users only.</h1>
+      <div>
+        <UserButton />
+      </div>
     </div>
   );
 }
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://precise-porpoise-92.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
