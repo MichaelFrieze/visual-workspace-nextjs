6ba1e85b007f32a8dcb1618590744fc8ce6c70e9
Michael Frieze <michaelfrieze@icloud.com>
2024-02-17 17:29:15 -0500

    feat: add empty states for dashboard favorites, org, search, and boards

---
+++ app/(dashboard)/_components/board-list.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { EmptySearch } from "./empty-search";
+import { EmptyBoards } from "./empty-boards";
+import { EmptyFavorites } from "./empty-favorites";
+
+interface BoardListProps {
+  orgId: string;
+  query: {
+    search?: string;
+    favorites?: string;
+  };
+}
+
+export const BoardList = ({ orgId, query }: BoardListProps) => {
+  const data = [];
+
+  if (!data?.length && query.search) {
+    return <EmptySearch />;
+  }
+
+  if (!data?.length && query.favorites) {
+    return <EmptyFavorites />;
+  }
+
+  if (!data?.length) {
+    return <EmptyBoards />;
+  }
+
+  return (
+    <div>
+      <h2 className="text-3xl">Very cool</h2>
+      <div className="mt-8 grid grid-cols-1 gap-5 pb-10 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6"></div>
+    </div>
+  );
+};
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,27 @@
-const DashboardPage = () => {
+"use client";
+
+import { useOrganization } from "@clerk/nextjs";
+
+import { EmptyOrg } from "./_components/empty-org";
+import { BoardList } from "./_components/board-list";
+
+interface DashboardPageProps {
+  searchParams: {
+    search?: string;
+    favorites?: string;
+  };
+}
+
+const DashboardPage = ({ searchParams }: DashboardPageProps) => {
+  const { organization } = useOrganization();
+
   return (
     <div className="h-[calc(100%-80px)] flex-1 p-6">
-      <h1>Dashboard Page</h1>
+      {!organization ? (
+        <EmptyOrg />
+      ) : (
+        <BoardList orgId={organization.id} query={searchParams} />
+      )}
     </div>
   );
 };
