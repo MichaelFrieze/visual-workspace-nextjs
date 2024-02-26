b1b24172297b15e528f0cf8f293953a85bea4b7c
Michael Frieze <michaelfrieze@icloud.com>
2024-02-26 14:51:51 -0500

    build: setup liveblocks

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -5,3 +5,9 @@ export const Info = () => {
     </div>
   );
 };
+
+export const InfoSkeleton = () => {
+  return (
+    <div className="absolute left-2 top-2 flex h-12 w-[300px] items-center rounded-md bg-white px-1.5 shadow-md" />
+  );
+};
