d96222643c4f1a478bce540f4bded0597dbda51d
Michael Frieze <michaelfrieze@icloud.com>
2024-03-03 14:24:54 -0500

    fix: search and filtering in dashboard

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -4,23 +4,20 @@ import { useOrganization } from "@clerk/nextjs";
 
 import { EmptyOrg } from "./_components/empty-org";
 import { BoardList } from "./_components/board-list";
+import { useSearchParams } from "next/navigation";
 
-interface DashboardPageProps {
-  searchParams: {
-    search?: string;
-    favorites?: string;
-  };
-}
-
-const DashboardPage = ({ searchParams }: DashboardPageProps) => {
+const DashboardPage = () => {
   const { organization } = useOrganization();
+  const searchParams = useSearchParams();
+  const search = searchParams.get("search") || undefined;
+  const favorites = searchParams.get("favorites") || undefined;
 
   return (
     <div className="h-[calc(100%-80px)] flex-1 p-6">
       {!organization ? (
         <EmptyOrg />
       ) : (
-        <BoardList orgId={organization.id} query={searchParams} />
+        <BoardList orgId={organization.id} query={{ search, favorites }} />
       )}
     </div>
   );
